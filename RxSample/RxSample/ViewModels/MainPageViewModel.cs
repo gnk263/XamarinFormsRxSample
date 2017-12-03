07dfa0c0a49451a388f6b6b8a5ede05fb1cabe31
gnk263 <gnk263@users.noreply.github.com>
2017-12-03 18:11:18 +0900

    プロジェクトを追加

---
+++ RxSample/RxSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+using Reactive.Bindings;
+
+namespace RxSample.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ReactiveProperty<long> Counter { get; }
+
+        public MainPageViewModel()
+        {
+            Counter = Observable
+                .Interval(TimeSpan.FromSeconds(1))
+                .ToReactiveProperty();
+        }
+    }
+}
