9d1163a36f2362773868135515e03f6777b8909f
gnk263 <gnk263@users.noreply.github.com>
2017-12-03 19:12:38 +0900

    文字列の一部に変数を使用してみる

--- RxSample/RxSample/ViewModels/MainPageViewModel.cs
+++ RxSample/RxSample/ViewModels/MainPageViewModel.cs
@@ -7,11 +7,17 @@ namespace RxSample.ViewModels
     public class MainPageViewModel
     {
         public ReactiveProperty<long> Counter { get; }
+        public ReactiveProperty<string> Counter2 { get; }
 
         public MainPageViewModel()
         {
             Counter = Observable
                 .Interval(TimeSpan.FromSeconds(1))
+                .ToReactiveProperty();
+
+            Counter2 = Observable
+                .Interval(TimeSpan.FromSeconds(1))
+                .Select(x => $"timer is {x} seconds.")
                 .ToReactiveProperty();
         }
     }
